8d37122d344352d81f710f713999bf06f883f515
Kent Fredric <kentfredric@gmail.com>
2013-08-22 20:14:48 +1200

    Build results of 4eb26e9 (on master)

--- lib/Dist/Zilla/App/Command/dumpphases.pm
+++ lib/Dist/Zilla/App/Command/dumpphases.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::App::Command::dumpphases::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::App::Command::dumpphases::VERSION = '0.2.0';
+  $Dist::Zilla::App::Command::dumpphases::VERSION = '0.2.1';
 }
 
 # ABSTRACT: Dump a textual representation of each phase's parts.
@@ -163,7 +163,7 @@ Dist::Zilla::App::Command::dumpphases - Dump a textual representation of each ph
 
 =head1 VERSION
 
-version 0.2.0
+version 0.2.1
 
 =head1 SYNOPSIS
 
