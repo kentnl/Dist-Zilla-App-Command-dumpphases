dcc242a6dee2ec4f5e039f4ff6832871e35acc9f
Kent Fredric <kentfredric@gmail.com>
2013-08-24 01:22:18 +1200

    Build results of 266546d (on master)

--- lib/Dist/Zilla/dumpphases/Theme/basic/red.pm
+++ lib/Dist/Zilla/dumpphases/Theme/basic/red.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Dist::Zilla::dumpphases::Theme::basic::red::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Dist::Zilla::dumpphases::Theme::basic::red::VERSION = '0.3.1';
+  $Dist::Zilla::dumpphases::Theme::basic::red::VERSION = '0.3.2';
 }
 
 # ABSTRACT: A red color theme for C<dzil dumpphases>
@@ -34,7 +34,7 @@ Dist::Zilla::dumpphases::Theme::basic::red - A red color theme for C<dzil dumpph
 
 =head1 VERSION
 
-version 0.3.1
+version 0.3.2
 
 =head1 SYNOPSIS
 
