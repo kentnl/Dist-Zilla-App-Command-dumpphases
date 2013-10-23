f64bef469c1418a9111f58b763b63ad6f0be3157
Kent Fredric <kentfredric@gmail.com>
2013-10-24 08:49:53 +1300

    Make before_script more obvious

--- before_script.pl
+++ before_script.pl
@@ -5,9 +5,10 @@ use warnings;
 use utf8;
 sub diag { print STDERR @_; print STDERR "\n" }
 sub env_exists { return exists $ENV{ $_[0] } }
-sub env_true { return ( env_exists( $_[0] ) and $ENV{ $_[0] } ) }
+sub env_true   { return ( env_exists( $_[0] ) and $ENV{ $_[0] } ) }
+sub env_is     { return ( env_exists( $_[0] ) and $ENV{ $_[0] } eq $_[1] ) }
 
-sub safe_exec {
+sub safe_exec_nonfatal {
   my ( $command, @params ) = @_;
   diag("running $command @params");
   my $exit = system( $command, @params );
@@ -16,23 +17,35 @@ sub safe_exec {
     my $high = $exit >> 8;
     warn "$command failed: $? $! and exit = $high , flags = $low";
     if ( $high != 0 ) {
-      exit $high;
+      return $high;
     }
     else {
-      exit 1;
+      return 1;
     }
   }
-  return 1;
+  return 0;
 }
 
-if ( -e './Build.PL' ) {
-  safe_exec( $^X, './Build.PL' );
-  safe_exec("./Build");
+sub safe_exec {
+  my ( $command, @params ) = @_;
+  my $result = safe_exec_nonfatal( $command, @params );
+  exit $result if $result != 0;
+}
+
+if ( env_is( 'TRAVIS_BRANCH', 'master' ) ) {
+  diag("before_script skipped, TRAVIS_BRANCH=master");
   exit 0;
 }
-if ( -e './Makefile.PL' ) {
-  safe_exec( $^X, './Makefile.PL' );
-  safe_exec("make");
-  exit 0;
+else {
+  if ( -e './Build.PL' ) {
+    safe_exec( $^X, './Build.PL' );
+    safe_exec("./Build");
+    exit 0;
+  }
+  if ( -e './Makefile.PL' ) {
+    safe_exec( $^X, './Makefile.PL' );
+    safe_exec("make");
+    exit 0;
+  }
 }
 
