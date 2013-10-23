de3c46bc4ec85a309c6f3618ad8d654cd49fe616
Kent Fredric <kentfredric@gmail.com>
2013-10-24 05:34:04 +1300

    Pesky parentheses

--- install_deps.pl
+++ install_deps.pl
@@ -5,8 +5,8 @@ use utf8;
 
 sub diag { print STDERR @_; print STDERR "\n" }
 sub env_exists { return exists $ENV{ $_[0] } }
-sub env_true { return env_exists( $_[0] ) and $ENV{ $_[0] } }
-sub env_is { return env_exists($_[0]) and $ENV{$_[0]} eq $_[1] }
+sub env_true   { return env_exists( $_[0] ) and $ENV{ $_[0] } }
+sub env_is     { return env_exists( $_[0] ) and $ENV{ $_[0] } eq $_[1] }
 
 sub safe_exec {
   my ( $command, @params ) = @_;
@@ -32,30 +32,36 @@ if ( not env_exists('TRAVIS') ) {
 }
 
 my (@params) = qw[ --quiet --notest --mirror http://cpan.metacpan.org/ --no-man-pages ];
-if ( env_true('DEVELOPER_DEPS') or env_is('TRAVIS_BRANCH','master') ) {
+if ( env_true('DEVELOPER_DEPS') or env_is( 'TRAVIS_BRANCH', 'master' ) ) {
   push @params, '--dev';
 }
-if ( env_is('TRAVIS_BRANCH', 'master' ) ) {
-    safe_exec('cpanm', @params, 'Dist::Zilla', 'Capture::Tiny');
-    require Capture::Tiny;
-    my $stdout = Capture::Tiny::capture_stdout(sub {
-        safe_exec('dzil', 'authordeps', '--missing');
-    });
-    safe_exec('cpanm', @params, split /\n/, $stdout );
-    $stdout = Capture::Tiny::capture_stdout(sub {
-        safe_exec('dzil','listdeps', '--missing');
-    });
-    safe_exec('cpanm', @params, split /\n/, $stdout );
-} else {
-    safe_exec( 'cpanm', @params, '--installdeps', '.' );
-    if ( env_true('AUTHOR_TESTING') or env_true('RELEASE_TESTING') ) {
-      require CPAN::Meta;
-      my $meta    = CPAN::Meta->load_file('META.json');
-      my $prereqs = $meta->effective_prereqs;
-      my $reqs    = $prereqs->requirements_for( 'develop', 'requires' );
-      for my $module ( sort $reqs->required_modules ) {
-        safe_exec( 'cpanm', @params, $module . '~' . $reqs->requirements_for_module($module) );
+if ( env_is( 'TRAVIS_BRANCH', 'master' ) ) {
+  safe_exec( 'cpanm', @params, 'Dist::Zilla', 'Capture::Tiny' );
+  require Capture::Tiny;
+  my $stdout = Capture::Tiny::capture_stdout(
+    sub {
+      safe_exec( 'dzil', 'authordeps', '--missing' );
     }
+  );
+  safe_exec( 'cpanm', @params, split /\n/, $stdout );
+  $stdout = Capture::Tiny::capture_stdout(
+    sub {
+      safe_exec( 'dzil', 'listdeps', '--missing' );
+    }
+  );
+  safe_exec( 'cpanm', @params, split /\n/, $stdout );
+}
+else {
+  safe_exec( 'cpanm', @params, '--installdeps', '.' );
+  if ( env_true('AUTHOR_TESTING') or env_true('RELEASE_TESTING') ) {
+    require CPAN::Meta;
+    my $meta    = CPAN::Meta->load_file('META.json');
+    my $prereqs = $meta->effective_prereqs;
+    my $reqs    = $prereqs->requirements_for( 'develop', 'requires' );
+    for my $module ( sort $reqs->required_modules ) {
+      safe_exec( 'cpanm', @params, $module . '~' . $reqs->requirements_for_module($module) );
+    }
+  }
 }
 
 exit 0;
